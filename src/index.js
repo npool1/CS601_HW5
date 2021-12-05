f917c9da185377b8d2236bf6b0b428563e46f538
Nick <nickpool@Adriennes-iMac.attlocal.net>
2021-12-05 12:22:02 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-async function getDegrees(url) {
-  // fetch the url
-  const response = await fetch(url)
-  console.log(response)
-    //get your data here, and check for the response status. If it's not 200, throw an error
-    // .then((response) => response.json())
-    // .then((college_degrees_earned) =>
-    //   document.write(
-    //     //`My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
-    //     `hello ${college_degrees_earned.college_degrees_earned[0].degree.School}`
-    //   )
-    // );
-}
-
 async function fetchDegrees(url) {
   const response = await fetch(url);
   if (!response.ok) {
@@ -19,12 +5,12 @@ async function fetchDegrees(url) {
     throw new Error(message);
   }
   const degrees = await response.json();
-  return degrees;
+  document.write(
+    `My first degree is from ${degrees.college_degrees_earned[0].School}`
+  )
 }
 
 fetchDegrees("./src/degres.json").catch(error => {
-  error.message; // 'An error has occurred: 404'
+  document.write(`${error.message}`) // 'An error has occurred: 404'
 });
 
-//fetchDegrees("./src/degres.json");
-//getDegrees("./src/degrees.json");
