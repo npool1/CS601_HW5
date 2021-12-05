02edc63585aa91a01052b663d51a4c768a95e48b
Nick <nickpool@Adriennes-iMac.attlocal.net>
2021-12-05 11:41:31 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 async function getDegrees(url) {
   // fetch the url
   const response = await fetch(url)
-  console.log(response.json())
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
-  }
+  console.log(response)
     //get your data here, and check for the response status. If it's not 200, throw an error
-    .then((response) => response.json())
-    .then((college_degrees_earned) =>
-      document.write(
-        //`My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
-        `hello ${college_degrees_earned.college_degrees_earned[0].degree.School}`
-      )
-    );
+    // .then((response) => response.json())
+    // .then((college_degrees_earned) =>
+    //   document.write(
+    //     //`My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
+    //     `hello ${college_degrees_earned.college_degrees_earned[0].degree.School}`
+    //   )
+    // );
 }
 
-getDegrees("./src/degree.json");
+getDegrees("./src/degrees.json");
