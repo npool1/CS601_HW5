1a567604439103deb2da771b22ee510d636b1ca8
Nick <nickpool@Adriennes-iMac.attlocal.net>
2021-12-05 11:04:43 -0500

    looking for response code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ async function getDegrees(url) {
   // fetch the url
   await fetch(url)
     //get your data here, and check for the response status. If it's not 200, throw an error
-    console.log(response.status)
     .then((response) => response.json())
+    console.log(response.status)
     .then((college_degrees_earned) =>
       document.write(
         //`My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
