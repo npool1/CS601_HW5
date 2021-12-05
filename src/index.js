af56d1f4c7b7b8301eb5eca1f09b51160cff0f29
Nick Pool <33762756+npool1@users.noreply.github.com>
2021-12-05 10:44:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ async function getDegrees(url) {
   await fetch(url)
     //get your data here, and check for the response status. If it's not 200, throw an error
     .then((response) => response.json())
-    .then((data) =>
+    .then((college_degrees_earned) =>
       document.write(
         //`My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
         `hello ${college_degrees_earned[0].degree.school}`
